5d1ad0eb7c7489018b60277e082ef64063602f54
Matthew Henschke <matthewhenschke98@gmail.com>
2020-08-12 17:34:02 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React-Boilerplate
+
+A react boilerplate repository that can be used for react apps without having to rely on the create-react-app tool.
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // used for building html that will have our react app
+
+module.exports = {
+  entry: "./src/index.js", // entry file
+  output: {
+    path: path.join(__dirname, "/build"), // index.js that will have all of our components will be compiled in build folder
+    filename: "bundle.js", // file name
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // look for js or jsx files, / is the delimiter
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+};
